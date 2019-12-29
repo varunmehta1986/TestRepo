b4ce3a2cdbeeba78fcdd739a5a173e8dab0f1888
varunmehta1986 <varun.mehta@generac.com>
2019-12-29 00:41:46 -0600

    First commit

---
+++ WCFPractice/Models/Movies.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace WCFPractice.Models
+{
+	[DataContract(Name = "Movie")]
+	public class Movies
+	{
+		public int Id { get; set; }
+		public string MovieName { get; set; }
+	}
+}
---
+++ WCFPractice/MyDataService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Services;
+using System.Data.Services.Common;
+using System.Linq;
+using System.Web;
+
+namespace WCFPractice
+{
+	public class MyDataService 
+	{
+		public static void Initialize(DataServiceConfiguration config)
+		{
+			config.SetEntitySetAccessRule("Movies", EntitySetRights.AllRead);
+			config.DataServiceBehavior.MaxProtocolVersion = DataServiceProtocolVersion.V2;
+		}
+	}
+}
